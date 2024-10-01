0bd43a0c81d535cc7d8c1787543a529d9e23bccb
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-10-01 08:35:32 +0700

    refac: movefile

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,11 +2,11 @@ import AboutMeComponent from '@/components/aboutme/AboutMe';
 import ContactsComponent from '@/components/contact/Contacts';
 import ExperiencesComponent from '@/components/experience/Experiences';
 import HomeContainer from '@/components/home/container';
+import VerticalTimeline from '@/components/home/VerticalTimeline';
 import IntroductionComponent from '@/components/introduction/Introduction';
 import JumperComponent from '@/components/jumper/Jumper';
 import NavBarComponent from '@/components/navbar/Navbar';
 import ProjectsComponent from '@/components/projects/Projects';
-import VerticalTimeline from '@/components/timeline/VerticalTimeline';
 
 export default function Home() {
   return (
